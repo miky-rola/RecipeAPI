42e78a177b1ba032494abc1c18439a91fbc4e9a3
miky-rola <mikyrola8@gmail.com>
2024-10-27 12:22:08 +0000

    feat: updated code

--- Template/reviews_routes.py
+++ Template/reviews_routes.py
@@ -2,7 +2,6 @@ from flask import Blueprint, jsonify, request, session
 from models import Review, Recipe, Users, db
 from user_routes import token_required
 
-# Creating a Blueprint for review-related routes
 review_blueprint = Blueprint("review_blueprint", __name__)
 
 
@@ -21,7 +20,6 @@ def leave_review(recipe_id):
 
         current_user_id = session.get("user_id")
 
-        # Create a new review object and associate it with the recipe
         new_review = Review(
             review_content=review_content,
             user_id=current_user_id if not is_anonymous else None,
@@ -29,15 +27,12 @@ def leave_review(recipe_id):
             is_anonymous=is_anonymous
         )
 
-        # Add new review to database
         db.session.add(new_review)
         db.session.commit()
 
-        # Return success message
         return jsonify({"message": "Review added successfully"}), 201
     except Exception as e:
         print("Error:", str(e))
-        # Return error message if an exception occurs
         return jsonify({"message": "An error occurred while adding the review"}), 500 
     
 
@@ -49,7 +44,6 @@ def get_recipe_reviews(recipe_name):
     if not recipe:
         return jsonify({"message": "Recipe not found"}), 404
     
-    # Query all reviews for the recipe
     reviews = Review.query.filter_by(recipe_id=recipe.id).all()
     review_data = []
     for review in reviews:
@@ -57,7 +51,6 @@ def get_recipe_reviews(recipe_name):
             reviewer = "Anonymous"
         else:
             reviewer = Users.query.get(review.user_id).username
-        # Construct response data for each review
         review_info = {
             "review_id": review.id,
             "review_content": review.review_content,
@@ -65,7 +58,6 @@ def get_recipe_reviews(recipe_name):
             "reviewer": reviewer
         }
         review_data.append(review_info)
-    # Return list of reviews
     return jsonify({"reviews": review_data}), 200
 
 @review_blueprint.route("/reviews/<int:review_id>", methods=["DELETE"])
@@ -76,12 +68,9 @@ def delete_review(review_id):
     if not review:
         return jsonify({"message": "Review not found"}), 404
     
-    # Check if the user is authorized to delete the review
     if review.user_id != session.get("user_id"):
         return jsonify({"message": "You are not authorized to delete this review"}), 403
 
-    # Delete review from database
     db.session.delete(review)
     db.session.commit()
-    # Return success message
     return jsonify({"message": "Review deleted successfully"}), 200
